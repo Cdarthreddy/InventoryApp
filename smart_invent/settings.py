c33f1b15530bb7f0cad056cd4c645ad4faed6525
Reddy <cdh20026@gmail.com>
2025-04-02 18:59:44 +0530

    Pylint Issues

--- smart_invent/settings.py
+++ smart_invent/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = get_random_secret_key()       
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_URL = "static/"   
+STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_ROOT = "staticfiles/"
@@ -133,5 +133,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login'
 
-LOGIN_REDIRECT_URL = '/'  
-
+LOGIN_REDIRECT_URL = '/'
